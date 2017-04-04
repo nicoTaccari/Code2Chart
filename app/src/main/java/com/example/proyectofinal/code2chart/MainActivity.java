21ca9f328a24f16567c73d380324c4eddebbfe71
Nicolas Taccari <nicolastaccari@gmail.com>
2017-04-04 17:12:53 -0300

    TODO setear el menu del panel de navegacion

--- app/src/main/java/com/example/proyectofinal/code2chart/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/code2chart/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.proyectofinal.code2chart;
 
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.io.File;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] pruebas;
+    private DrawerLayout drawerLayout;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File nuevaCarpeta = new File(getFilesDir(), "Code2Chart");
-        if(!nuevaCarpeta.exists()) {
-            nuevaCarpeta.mkdirs();
-        }
+        pruebas = getResources().getStringArray(R.array.nombres)
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView = (ListView) findViewById(R.id.left_drawer);
+
+        //listView.setAdapter(new ArrayAdapter<String>(this,R.layout., pruebas));
+        // Set the list's click listener
+        listView.setOnItemClickListener(new DrawerItemClickListener());
 
     }
+
 }
