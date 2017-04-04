d92f4d45cd0062a418944d8523ee7a26a3671780
santiagojvarela <santiagojvarela@hotmail.com.ar>
2017-04-04 14:40:30 -0300

    Agrego posible cambio de Barra para emular

--- app/src/main/java/com/example/proyectofinal/code2chart/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/code2chart/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.proyectofinal.code2chart;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.barra_titulo);
     }
 }
