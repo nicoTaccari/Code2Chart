9ec677a605c7570aacedae7a9297be6d6ad9cdba
Rodrigo Vazquez <rodovazquez1@gmail.com>
2017-04-04 15:16:06 -0300

    Barra superior con nombre. Logos.

--- app/src/main/java/com/example/proyectofinal/code2chart/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/code2chart/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.proyectofinal.code2chart;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.barra_titulo);
     }
 }
