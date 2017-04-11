b0c7fcc383b4b0b3abbe1d295e075bc4df4e3278
Rodrigo Vazquez <rodovazquez1@gmail.com>
2017-04-11 17:21:39 -0300

    Delegar selección del diagrama al FileSystem. Atrapar la URI.

--- app/src/main/java/com/example/proyectofinal/code2chart/CrearDiagrama.java
+++ app/src/main/java/com/example/proyectofinal/code2chart/CrearDiagrama.java
@@ -1,17 +1,104 @@
 package com.example.proyectofinal.code2chart;
 
 import android.app.ActionBar;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CrearDiagrama extends AppCompatActivity {
+import java.net.URISyntaxException;
+
+public class CrearDiagrama extends AppCompatActivity implements View.OnClickListener{
+
+    Button boton;
+    TextView uriTexto;
+    private static final int FILE_SELECT_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        boton = (Button) findViewById(R.id.button2);
+        boton.setOnClickListener(this);
+
+        uriTexto = (TextView) findViewById(R.id.uri);
+
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Uri uri = data.getData();
+                        //uriTexto.setText(uri.getPath());
+                        uriTexto.setText(getPath(CrearDiagrama.this, uri));
+                    } catch (Exception e) {
+                        // Eat it
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static String getPath(Context context, Uri uri) throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = { "_data" };
+
+            try {
+                Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null); //TODO el cursor devuelve nulo. ¿Por qué?
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+                cursor.close();
+            } catch (Exception e) {
+                // Eat it
+            }
+        }
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        showFileChooser();
 
     }
 }
