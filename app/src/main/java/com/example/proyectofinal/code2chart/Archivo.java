eb66fd23d69ca059dccaa9d1c3ea49aca941cd17
Nicolas Taccari <nicolastaccari@gmail.com>
2017-04-18 15:09:05 -0300

    Eliminado el uso de carpetas. Al perder foco, el searchview se cierra.

--- app/src/main/java/com/example/proyectofinal/code2chart/Archivo.java
+++ app/src/main/java/com/example/proyectofinal/code2chart/Archivo.java
@@ -1,19 +1,31 @@
 package com.example.proyectofinal.code2chart;
 
+import android.content.Context;
+import android.widget.ListView;
+
 /**
  * Created by nico on 11/04/17.
  */
 
-public class Archivo extends FileComposite {
+public class Archivo {
+
+    public int getIcono() {
+        return icono;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    private int icono;
+    private String titulo;
 
     public Archivo (String untitulo, int unIcono) {
         this.icono = unIcono;
         this.titulo = untitulo;
     }
 
-    @Override
-    public void abrir() {
-
+    public void abrir(Context context, ListView listaDeArchivos) {
+        //TODO implementar cuando haya archivos para mostrar
     }
-
 }
